e8c0d88ac3170c8fac2f419712870b5d379056d5
Markus Hauge <markus.hauge@hotmail.com>
2022-12-09 22:46:00 +0100

    Allow closures to capture references

--- src/lib.rs
+++ src/lib.rs
@@ -3,41 +3,56 @@ use std::{
     collections::HashMap,
 };
 
-type EventListener = dyn FnMut(&dyn Any);
+type EventListener<'a> = dyn FnMut(&dyn Any) + 'a;
 
 #[derive(Default)]
-pub struct EventDispatcher {
-    listeners: HashMap<TypeId, HashMap<TypeId, Box<EventListener>>>,
+pub struct EventDispatcher<'a> {
+    listeners: HashMap<TypeId, Vec<Box<EventListener<'a>>>>,
 }
 
-impl EventDispatcher {
+impl<'a> EventDispatcher<'a> {
     pub fn new() -> Self {
         Self::default()
     }
 
-    pub fn add_listener<E: 'static, L: FnMut(&E) + 'static>(&mut self, mut listener: L) {
-        self.listeners.entry(TypeId::of::<E>()).or_default().insert(
-            TypeId::of::<L>(),
-            Box::new(move |event| {
-                if let Some(event) = event.downcast_ref::<E>() {
-                    listener(event);
-                }
-            }),
-        );
-    }
-
-    pub fn remove_listener<E: 'static, L: FnMut(&E) + 'static>(&mut self, _: L) {
+    pub fn add_listener<E: 'static, L: FnMut(&E) + 'a>(&mut self, mut listener: L) {
         self.listeners
             .entry(TypeId::of::<E>())
             .or_default()
-            .remove(&TypeId::of::<L>());
+            .push(Box::new(move |event| {
+                if let Some(event) = event.downcast_ref::<E>() {
+                    listener(event);
+                }
+            }));
     }
 
     pub fn dispatch<E: 'static>(&mut self, event: &E) {
         if let Some(listeners) = self.listeners.get_mut(&TypeId::of::<E>()) {
-            for listener in listeners.values_mut() {
+            for listener in listeners {
                 listener(event);
             }
         };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::cell::RefCell;
+
+    use crate::EventDispatcher;
+
+    #[test]
+    fn it_works() {
+        struct Event;
+
+        let event_count = RefCell::new(0);
+        let mut dispatcher = EventDispatcher::new();
+        dispatcher.add_listener(|_: &Event| *event_count.borrow_mut() += 1);
+
+        dispatcher.dispatch(&Event);
+        assert_eq!(*event_count.borrow(), 1);
+
+        dispatcher.dispatch(&Event);
+        assert_eq!(*event_count.borrow(), 2);
     }
 }
